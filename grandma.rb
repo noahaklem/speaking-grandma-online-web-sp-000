f6fd7e80af48879599b2dda10d77faa2fbb8b269
Noah  Klem <klem.noah@gmail.com>
2021-05-25 02:18:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,9 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!' 
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase 
-    return "HUH?! SPEAK UP, SONNY!"
+    return "NO, NOT SINCE 1938!" 
   else 
-    return "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
