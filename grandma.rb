9a3bd7a659bc855970a420d868972c8ce66fd173
Noah  Klem <klem.noah@gmail.com>
2021-05-25 02:17:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,11 @@
 # Write a speak_to_grandma method.
-require 'pry'
+
 
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!' 
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase 
     return "HUH?! SPEAK UP, SONNY!"
-    binding.pry
   else 
     return "NO, NOT SINCE 1938!"
   end
